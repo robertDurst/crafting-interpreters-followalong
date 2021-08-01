f6008d87f8e762679ecc5e74cf17bf79aaab8e7a
Rob Durst <me@robdurst.com>
2021-08-01 04:34:26 -0700

    more progress on Scanner

--- Granola/Lexar/Scanner.cs
+++ Granola/Lexar/Scanner.cs
@@ -1,4 +1,42 @@
+using System.Collections.Generic;
+
 namespace Granola.Lexar
 {
-    
+    public class Scanner
+    {
+        private readonly string _source;
+        private List<Token> _tokens;
+
+        private int _currentIndex;
+        private int _startIndex;
+        private int _currentLine;
+
+        public Scanner(string source)
+        {   
+            _source = source;
+            _tokens = new List<Token>();
+        }
+
+        public List<Token> ScanTokens()
+        {
+            while (!IsAtEnd())
+            {
+                _startIndex = _currentIndex;
+                ScanToken();
+            }
+
+            _tokens.Add(new Token());
+            return _tokens;
+        }
+
+        private Token ScanToken()
+        {
+            return new Token();
+        }
+
+        private bool IsAtEnd()
+        {
+            return _currentIndex >= _source.Length;
+        }
+    }
 }
